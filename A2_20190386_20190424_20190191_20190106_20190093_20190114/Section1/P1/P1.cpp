0893fbc9af9493979521e1b29102c9d68d94bfdc
dovaaa-emad <77188095+dovaaa-emad@users.noreply.github.com>
2021-06-09 16:06:15 +0200

    final S1P1

--- A2_20190386_20190424_20190191_20190106_20190093_20190114/Section1/P1/P1.cpp
+++ A2_20190386_20190424_20190191_20190106_20190093_20190114/Section1/P1/P1.cpp
@@ -2,9 +2,12 @@
 #include <iomanip>
 #include <chrono>
 #include <vector>
+#include <fstream>
 #include <time.h>
 using namespace std;
 using namespace std::chrono;
+
+
 
 void print_arr(int nums[], int length){
     for(int i = 0; i < length; i++){
@@ -38,7 +41,7 @@ void merge(int nums[], int start, int mid, int end) {
             nums[k] = right[j];
             j++;
         }
-        
+
         k++;
     }
 
@@ -68,7 +71,7 @@ void mergeSort(int nums[], int start, int end) {
 void mergeSort(int nums[], int length) {
     mergeSort(nums, 0, length - 1);
     cout << "Sorted By Merge Sort: ";
-    print_arr(nums, length);
+    //print_arr(nums, length);
 }
 
 int partitionMiddle(int nums[], int start, int end, int pivot){
@@ -80,7 +83,7 @@ int partitionMiddle(int nums[], int start, int end, int pivot){
         while(nums[end] > pivot){
             end--;
         }
-        
+
         if(start <= end){
             swap(nums[start], nums[end]);
             start++;
@@ -95,9 +98,9 @@ int partitionMiddle(int nums[], int start, int end, int pivot){
 void quickSortMiddlePivot(int nums[], int start, int end){
     if(start < end){
         int pivot = nums[(start + end) / 2];
-        
+
         int pivotInRightPlaceIndex = partitionMiddle(nums, start, end, pivot);
-        
+
         quickSortMiddlePivot(nums, start, pivotInRightPlaceIndex - 1);
         quickSortMiddlePivot(nums, pivotInRightPlaceIndex, end);
 
@@ -107,17 +110,17 @@ void quickSortMiddlePivot(int nums[], int start, int end){
 void quickSortMiddlePivot(int nums[], int length) {
     quickSortMiddlePivot(nums, 0, length - 1);
     cout << "Sorted By Quick Sort: ";
-    print_arr(nums, length);
+    //print_arr(nums, length);
 }
 
 
 
 
 int partitionEnd(int nums[], int low, int high) {
-    
+
   // select the rightmost element as pivot
   int pivot = nums[high];
-  
+
   // pointer for greater element
   int i = low;
 
@@ -125,28 +128,28 @@ int partitionEnd(int nums[], int low, int high) {
   // compare them with the pivot
   for (int j = low; j < high; j++) {
     if (nums[j] <= pivot) {
-        
+
       // if element smaller than pivot is found
       // swap it with the greater element pointed by i
-      
+
       // swap element at i with element at j
       swap(nums[i], nums[j]);
       i++;
     }
   }
-  
+
   // swap pivot with the greater element at i
   swap(nums[i], nums[high]);
-  
+
   // return the partition point
   return i;
 }
 
 void quickSortEndPivot(int nums[], int start, int end){
     if(start < end){
-        
+
         int pivotInRightPlaceIndex = partitionEnd(nums, start, end);
-        
+
         quickSortEndPivot(nums, start, pivotInRightPlaceIndex - 1);
         quickSortEndPivot(nums, pivotInRightPlaceIndex + 1, end);
 
@@ -160,54 +163,6 @@ void quickSortEndPivot(int nums[], int length) {
     print_arr(nums, length);
 }
 
-// int partitionStart(int nums[], int low, int high) {
-    
-//   // select the leftmost element as pivot
-//   int pivot = nums[low];
-  
-//   // pointer for greater element
-//   int i = low;
-
-//   // traverse each element of the array
-//   // compare them with the pivot
-//   for (int j = low; j < high; j++) {
-//     if (nums[j] <= pivot) {
-        
-//       // if element smaller than pivot is found
-//       // swap it with the greater element pointed by i
-      
-//       // swap element at i with element at j
-//       swap(nums[i], nums[j]);
-//       i++;
-//     }
-//   }
-  
-//   // swap pivot with the greater element at i
-//   swap(nums[i], nums[high]);
-  
-//   // return the partition point
-//   return i;
-// }
-
-
-
-// void quickSortStartPivot(int nums[], int start, int end){
-//     if(start < end){
-        
-//         int pivotInLeftPlaceIndex = partitionStart(nums, start, end);
-        
-//         quickSortStartPivot(nums, start, pivotInLeftPlaceIndex - 1);
-//         quickSortStartPivot(nums, pivotInLeftPlaceIndex + 1, end);
-
-//     }
-// }
-
-
-// void quickSortStartPivot(int nums[], int length) {
-//     quickSortStartPivot(nums, 0, length - 1);
-//     cout << "Sorted By Quick Sort: ";
-//     print_arr(nums, length);
-// }
 
 
 
@@ -217,30 +172,85 @@ void randGenerator(int nums[],int length){
 }
 
 
+
+
 int main(){
-    int t;
-    int size;
-    cout<<"Number of tests + Number of elements\n";
-    cin>>t>>size;
-    while(t){
-    
-    int nums1[size];
-    int nums2[size];
-    randGenerator(nums1,size);
-    for(int i = 0;i<size;i++){
-        nums2[i]=nums1[i];
-    }
+
+    // ios_base::sync_with_stdio(0);
+    // cin.tie();
+
+
+    int nums1[5000];
+    int nums2[10000];
+    int nums3[100000];
+
+    randGenerator(nums1,5000);
+    randGenerator(nums2,10000);
+    randGenerator(nums3,100000);
+
+
     auto start1 = high_resolution_clock::now();
-    mergeSort(nums1,size);
+    mergeSort(nums1,5000);
     auto stop1 = high_resolution_clock::now();
     auto duration1 = duration_cast<microseconds>(stop1 - start1);
     cout<<"mergeSort time Taken: " << duration1.count() << " microseconds\n\n";
 
+ofstream time("Time.csv");
+time<<duration1.count()<<",";
+
+    start1 = high_resolution_clock::now();
+    mergeSort(nums2,10000);
+    stop1 = high_resolution_clock::now();
+    duration1 = duration_cast<microseconds>(stop1 - start1);
+    cout<<"mergeSort time Taken: " << duration1.count() << " microseconds\n\n";
+time<<duration1.count()<<",";
+
+     start1 = high_resolution_clock::now();
+    mergeSort(nums3,100000);
+     stop1 = high_resolution_clock::now();
+     duration1 = duration_cast<microseconds>(stop1 - start1);
+    cout<<"mergeSort time Taken: " << duration1.count() << " microseconds\n\n";
+
+time<<duration1.count()<<endl;
+
+
+int nums4[5000];
+
+int nums5[10000];
+
+int nums6[100000];
+
+
+randGenerator(nums4,5000);
+randGenerator(nums5,10000);
+randGenerator(nums6,100000);
+
+
+
     auto start2 = high_resolution_clock::now();
-    quickSortMiddlePivot(nums2,size);
+    quickSortMiddlePivot(nums4,5000);
     auto stop2 = high_resolution_clock::now();
     auto duration2= duration_cast<microseconds>(stop2 - start2);
     cout<<"quickSort time Taken: " << duration2.count()  << " microseconds\n";
-    t--;}
+
+time<<duration2.count()<<",";
+
+    auto start3 = high_resolution_clock::now();
+    quickSortMiddlePivot(nums5,10000);
+    auto stop3 = high_resolution_clock::now();
+    auto duration3= duration_cast<microseconds>(stop3 - start3);
+    cout<<"quickSort time Taken: " << duration3.count()  << " microseconds\n";
+
+time<<duration3.count()<<",";
+
+    auto start4 = high_resolution_clock::now();
+    quickSortMiddlePivot(nums6,100000);
+    auto stop4 = high_resolution_clock::now();
+    auto duration4= duration_cast<microseconds>(stop4 - start4);
+    cout<<"quickSort time Taken: " << duration4.count()  << " microseconds\n";
+
+time<<duration4.count()<<endl;
+
+time.close();
     return 0;
 }
